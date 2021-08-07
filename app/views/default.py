3c85bbc2e810dee0ee763eeb108e5689a45f0a9d
tidaybreak <tidaybreak@163.com>
2021-08-07 22:39:57 +0800

    bug

--- app/views/default.py
+++ app/views/default.py
@@ -1,85 +1,82 @@
-from flask import Flask, request, jsonify, Response, make_response
+# coding=utf-8
+
+
 import requests
-from units import get_cache_file, save_cache_file, is_valid_domain
-from config import cfg
 import hashlib
-import logging
-import sys, getopt
-import re
+from flask import Blueprint, request, make_response
+from app.ext import serv, app
+from app.utils import get_cache_file, save_cache_file, is_valid_domain
+from app.config import cfg
 
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
-
-
-def create_app():
-    app = Flask(__name__)
-    app.config['JSON_AS_ASCII'] = False
-    return app
-
-
-app = create_app()
-
-# 忽略参数的url
-query_urls = []
-cache_code = [200, 302]
+default = Blueprint('default', __name__)
 
 
 @app.before_request
-def proxy():
-    headers = {h[0]: h[1] for h in request.headers}
-    url = request.url
-    real_host = request.values.get('real_host', cfg.DOMAIN)
-    real_full_path = request.full_path
+def catch_all():
+    if request.url.find('hsf/realtime/focusIndicators') < 0:
+        # print(url)
+        pass
 
-    # 移除一些随机参数
+    headers = {h[0]: h[1] for h in request.headers}
+
+    # 处理path 移除一些随机参数
+    full_path = request.full_path
     for args in cfg.REMOVE_ARGS:
         val = request.args.get(args)
         if val:
             remove_args = args + '=' + val
-            real_full_path = real_full_path.replace('&' + remove_args, '')
-            real_full_path = real_full_path.replace(remove_args, '')
+            full_path = full_path.replace('&' + remove_args, '')
+            full_path = full_path.replace(remove_args, '')
 
-    # 如果第一个是域名
+    # 获取真实host host可能在path[1]
+    real_host = request.values.get('real_host', cfg.DOMAIN)
     paths = request.path.split('/')
     if len(paths) > 2 and is_valid_domain(paths[1]):
         real_host = paths[1]
-        real_full_path = request.full_path.replace('/'+real_host, '', 1)
-
-    if url.find('hsf/realtime/focusIndicators') < 0:
-        # print(url)
-        pass
+        full_path = request.full_path.replace('/' + real_host, '', 1)
 
     # cookies = {h: request.cookies.get(h) for h in request.cookies}
 
-    host_url = cfg.HTTP + real_host
+    # 获取真实请求url
     headers['Host'] = real_host
     # Origin要判断再设置
     if 'Origin' in headers:
-        headers['Origin'] = host_url
-    headers['Referer'] = headers.get("Referer", '').replace(request.host_url, host_url + '/')
-    p_url = host_url + real_full_path
+        headers['Origin'] = cfg.HTTP + real_host
+    headers['Referer'] = headers.get("Referer", '').replace(request.host_url, cfg.HTTP + real_host + '/')
+    read_url = cfg.HTTP + real_host + full_path
 
-    contain_query = True
-    if request.path in query_urls:
-        contain_query = False
-
-    post_data = request.data
-
+    # byte或dict
+    post_data = b''
     # Post Content-Type一般是 text/plain  application/x-www-form-urlencoded
-    post_data_byte = b''
+    hash_data = b''
     if 'Content-Type' in headers:
         if headers['Content-Type'].find('application/x-www-form-urlencoded') >= 0:
             post_data = request.form.to_dict()
-            post_data_byte = bytes(str(post_data), encoding='utf-8')
+            hash_data = bytes(str(post_data), encoding='utf-8')
         else:
-            post_data_byte = request.data
+            post_data = request.data
+            hash_data = request.data
     post_data_hash = ''
-    if len(post_data_byte) > 0:
-        post_data_hash = hashlib.md5(post_data_byte).hexdigest()
+    if len(hash_data) > 0:
+        post_data_hash = hashlib.md5(hash_data).hexdigest()
 
-    cache_file, status_code, header, cookies, data = get_cache_file(p_url, post_data_hash, contain_query=contain_query)
+    # 忽略参数维度的url
+    contain_query = True
+    if request.path in cfg.QUERY_PATH:
+        contain_query = False
 
-    #if True:
+    get_cache = True
+    if request.path in cfg.NO_CACHE_READ_PATH:
+        get_cache = False
+    if len(cfg.NO_CACHE_READ_PATH) == 1 and cfg.NO_CACHE_READ_PATH[0] == "*":
+        get_cache = False
+    if get_cache:
+        cache_file, status_code, header, cookies, data = get_cache_file(read_url, post_data_hash,
+                                                                        contain_query=contain_query)
+    else:
+        cache_file, status_code, header, cookies, data = 'skip ', 404, None, None, None
+
+    # if True:
     if data is None:
         try:
             status_code = 502
@@ -90,14 +87,14 @@ def proxy():
             headers.pop("If-None-Match", None)
             headers.pop("If-Modified-Since", None)
             headers = {**headers, **cfg.REQ_HEADER}
-            result = requests.request(request.method, p_url,
+            result = requests.request(request.method, read_url,
                                       data=post_data,
                                       headers=headers,
                                       allow_redirects=False,
                                       timeout=(8, 8))
             cookies = requests.utils.dict_from_cookiejar(result.cookies)
             if result.status_code in cfg.CACHE_CODES:
-                save_cache_file(p_url, post_data_hash, result.content,
+                save_cache_file(read_url, post_data_hash, result.content,
                                 status_code=result.status_code,
                                 header=result.headers,
                                 cookies=cookies,
@@ -106,11 +103,11 @@ def proxy():
             header = result.headers
             data = result.content
         except Exception as error:
-            print("error:", p_url, str(error))
+            print("error:", read_url, str(error))
 
-    # content = str(result.content, encoding='utf-8')
-    # content = content.replace('友盟', '昊盟')
-    # content = bytes(content, encoding='utf-8')
+    # 改写数据
+    data = serv.main.hook_response(request.path, full_path, post_data, data)
+
     response = make_response(data)
     for k in header:
         lk = k.lower()
@@ -128,7 +125,7 @@ def proxy():
         response.headers[k] = header.get(k)
 
     print(f"info:, {status_code}  {cache_file}  {cookies}")
-    # print(f"info:, {status_code}  {p_url}  {cookies}")
+    # print(f"info:, {status_code}  {read_url}  {cookies}")
 
     for k, v in cookies.items():
         response.set_cookie(k, v)
@@ -142,6 +139,7 @@ def proxy():
     return response, status_code
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=cfg.PORT)
-    # app.run(host='0.0.0.0', port=443, ssl_context=('keys/secret.pem', 'keys/secret.key'))
+@default.route("/other", methods=["GET", "POST"])
+def get_header():
+    # catch_all return 才会到这里
+    return ''
