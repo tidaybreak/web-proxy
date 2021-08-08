3370944575af2888edffd37c254ceeb5419cee5d
tidaybreak <tidaybreak@163.com>
2021-08-08 23:23:04 +0800

    bug

--- app/plugins/response/cdnplus.py
+++ app/plugins/response/cdnplus.py
@@ -5,7 +5,52 @@ __author__ = 'ti'
 
 import time
 import os
+import datetime
+import json
+import random
+import urllib.parse
 from app.utils import export
+from urllib.parse import urlparse
+
+
+def get_data(url, y="domain"):
+    url = url.replace('[]', '')
+    query = dict(urllib.parse.parse_qsl(urllib.parse.urlsplit(url).query))
+    domains = url.split('&domain%5B%5D=')
+    del domains[0]
+    domains[-1] = domains[-1].split('&')[0]
+    start_time = query['start_time']
+    end_time = query['end_time']
+    #type = query['type']
+
+    data = {
+        "code": 0,
+        "message": "",
+        "data": [
+
+        ]
+    }
+
+    if y == "domain":
+        for domain in domains:
+            item = [domain, random.randint(100000000, 900000000)]
+            data["data"].append(item)
+    elif y == "code":
+        for code in [200, 301, 404, 500]:
+            item = [code, random.randint(1000000, 9000000)]
+            data["data"].append(item)
+    elif y == "time":
+        num = 13
+        start_time = datetime.datetime.strptime(start_time, "%Y%m%d%H%M")
+        end_time = datetime.datetime.strptime(end_time, "%Y%m%d%H%M")
+        increment = int((end_time.timestamp() - start_time.timestamp()) / num)
+        for i in range(1, num):
+            stamp = int(start_time.timestamp()) + i * increment
+            item = [time.strftime("%Y-%m-%d %H:%M", time.localtime(stamp)), random.randint(100000000, 900000000)]
+            data["data"].append(item)
+
+    data = json.dumps(data)
+    return data
 
 
 @export("/statistic/summary/")
@@ -34,113 +79,48 @@ def statistic_summary(data, *args, **kwargs):
 
 @export("/statistic/domain-bandwidth/")
 def statistic_domain_bandwidth(data, *args, **kwargs):
-    data = '''
-        {
-        "code": 0,
-        "message": "",
-        "data": [["2021-08-07 17:55",
-        10],
-        ["2021-08-07 18:00",
-        20],
-        ["2021-08-07 18:05",
-        30],
-        ["2021-08-07 18:10",
-        40],
-        ["2021-08-07 18:15",
-        50],
-        ["2021-08-07 18:20",
-        60],
-        ["2021-08-07 18:25",
-        20],
-        ["2021-08-07 18:30",
-        30],
-        ["2021-08-07 18:35",
-        10],
-        ["2021-08-07 18:40",
-        40],
-        ["2021-08-07 18:45",
-        20],
-        ["2021-08-07 18:50",
-        30],
-        ["2021-08-07 18:55",
-        40]]
-    }
-    '''
-
+    data = get_data(args[0][0], "time")
     return bytes(data, encoding='utf-8')
 
 
 @export("/statistic/node-bandwidth-ranking/")
 def statistic_node_bandwidth_ranking(data, *args, **kwargs):
-    data = '''
-        {
-        "code": 0,
-        "message": "",
-        "data": [["2021-08-07 17:55",
-        10],
-        ["2021-08-07 18:00",
-        20],
-        ["2021-08-07 18:05",
-        30],
-        ["2021-08-07 18:10",
-        40],
-        ["2021-08-07 18:15",
-        50],
-        ["2021-08-07 18:20",
-        60],
-        ["2021-08-07 18:25",
-        20],
-        ["2021-08-07 18:30",
-        30],
-        ["2021-08-07 18:35",
-        10],
-        ["2021-08-07 18:40",
-        40],
-        ["2021-08-07 18:45",
-        20],
-        ["2021-08-07 18:50",
-        30],
-        ["2021-08-07 18:55",
-        40]]
-    }
-    '''
-
+    data = get_data(args[0][0])
     return bytes(data, encoding='utf-8')
 
 
 @export("/statistic/query-domain-ranking/")
 def statistic_query_domain_ranking(data, *args, **kwargs):
-    data = '''
-        {
-        "code": 0,
-        "message": "",
-        "data": [["2021-08-07 17:55",
-        10],
-        ["2021-08-07 18:00",
-        20],
-        ["2021-08-07 18:05",
-        30],
-        ["2021-08-07 18:10",
-        40],
-        ["2021-08-07 18:15",
-        50],
-        ["2021-08-07 18:20",
-        60],
-        ["2021-08-07 18:25",
-        20],
-        ["2021-08-07 18:30",
-        30],
-        ["2021-08-07 18:35",
-        10],
-        ["2021-08-07 18:40",
-        40],
-        ["2021-08-07 18:45",
-        20],
-        ["2021-08-07 18:50",
-        30],
-        ["2021-08-07 18:55",
-        40]]
-    }
-    '''
+    data = get_data(args[0][0])
+    return bytes(data, encoding='utf-8')
 
+
+@export("/statistic/query-domain-url-ranking/")
+def statistic_query_domain_url_ranking(data, *args, **kwargs):
+    data = get_data(args[0][0])
+    return bytes(data, encoding='utf-8')
+
+
+# 状态码
+@export("/statistic/query-code/")
+def statistic_query_code(data, *args, **kwargs):
+    data = get_data(args[0][0], y="code")
+    return bytes(data, encoding='utf-8')
+
+
+@export("/statistic/query-code-ranking/")
+def statistic_query_code_ranking(data, *args, **kwargs):
+    data = get_data(args[0][0], y="code")
+    return bytes(data, encoding='utf-8')
+
+
+@export("/statistic/query-hit/")
+def statistic_query_hit(data, *args, **kwargs):
+    data = get_data(args[0][0], y="time")
+    return bytes(data, encoding='utf-8')
+
+
+@export("/statistic/query-ip-ranking/")
+def statistic_query_ip_ranking(data, *args, **kwargs):
+    data = get_data(args[0][0], y="domain")
     return bytes(data, encoding='utf-8')
