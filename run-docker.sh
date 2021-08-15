16955f8a6e95adcd6e39781e26f47e27b567f3dd
tidaybreak <tidaybreak@163.com>
2021-08-15 17:24:57 +0800

    bug

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+curr_dir=$(pwd)
+chmod 777 $curr_dir
+
 docker stop web-proxy
 docker rm web-proxy
 docker run --name web-proxy -d \
@@ -7,11 +10,11 @@ docker run --name web-proxy -d \
 -e PYTHONUNBUFFERED=1 \
 -e TERM=linux \
 -e C_FORCE_ROOT=true \
--e APP_CONFIG= $1 \
+-e APP_CONFIG=$1 \
 --net mynet \
 --ip 172.18.0.135 \
 -p 80:5000 \
 --mount type=bind,source=/etc/resolv.conf,target=/etc/resolv.conf \
---mount type=bind,source=/home/data/docker-volume/web-proxy/,target=/opt/app-root/src \
+--mount type=bind,source=$curr_dir,target=/opt/app-root/src \
 centos/python-36-centos7:latest \
 /bin/sh /opt/app-root/src/run.sh
--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 cd /opt/app-root/src
 if [ ! -d "./venv" ]; then
   virtualenv --no-site-packages venv
+  source ./venv/bin/activate
+  pip install --upgrade pip
 fi
 
 source ./venv/bin/activate
